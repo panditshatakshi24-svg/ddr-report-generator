956f8a42fd896ec7cdaba9ad5b41c1c96a4c7b05
panditshatakshi24-svg <panditshatakshi24@gmail.com>
2026-04-16 22:16:35 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# ... other code ...
-
-# Change made here
-# original line 42: st.error("Please enter your OpenAI API key in the sidebar.")
-st.error("Please enter your Groq API key in the sidebar.")
-
-# ... other code ...
